efc4c306145ebbdb1f83428b7c1b925708b3621a
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-05-20 18:41:01 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Victor Marquez on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
