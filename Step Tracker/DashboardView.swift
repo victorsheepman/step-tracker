5617f5241d43bd2854e021fb7a11ec81b081bb99
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-05-22 18:28:29 -0400

    improve forEach

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -29,9 +29,8 @@ struct DashboardView: View {
             ScrollView {
                 VStack(spacing:20) {
                     Picker("Selected Stat", selection: $selectedStat) {
-                        ForEach(HealthMetricContext.allCases){ metric in
-                            Text(metric.title)
-                            
+                        ForEach(HealthMetricContext.allCases){
+                            Text($0.title)
                         }
                     }.pickerStyle(.segmented)
                     VStack {
