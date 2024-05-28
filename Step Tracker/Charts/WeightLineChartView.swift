dafe38fc558c3ca2934304bf4d8287affed98fc9
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-05-28 18:53:21 -0400

    weight line chart

--- Step Tracker/Charts/WeightLineChartView.swift
+++ Step Tracker/Charts/WeightLineChartView.swift
@@ -13,6 +13,10 @@ struct WeightLineChartView: View {
     var selectedStat: HealthMetricContext
     var chartData: [HealthMetric]
     
+    var minValue: Double {
+        chartData.map { $0.value }.min() ?? 0
+    }
+    
     var body: some View {
         VStack {
             NavigationLink(value: selectedStat) {
@@ -36,14 +40,46 @@ struct WeightLineChartView: View {
             .padding(.bottom, 12)
             
             Chart {
+                
+                RuleMark(y: .value("Goal", 155))
+                    .foregroundStyle(.mint)
+                    .lineStyle(.init(lineWidth: 1, dash: [5]))
+                    
+                
+                
                 ForEach(chartData){ weights in
                     
-                    AreaMark(x: .value("Day", weights.date, unit: .day), y: .value("Value", weights.value))
-                        .foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
-                    
+                    AreaMark(
+                        x:.value("Day", weights.date, unit: .day),
+                        yStart: .value("Value", weights.value),
+                        yEnd: .value("Min Value", minValue)
+                    )
+                    .foregroundStyle(Gradient(colors: [.indigo.opacity(0.5), .clear]))
+                    .interpolationMethod(.catmullRom)
+                 
                     LineMark(x: .value("Day", weights.date, unit: .day), y: .value("Value", weights.value))
+                        .foregroundStyle(.indigo)
+                        .interpolationMethod(.catmullRom)
+                        .symbol(.circle)
                 }
             }.frame(height: 150)
+            .chartYScale(domain: .automatic(includesZero: false))
+            .chartXAxis{
+                AxisMarks {value in
+                    AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+                }
+                    
+            }
+            .chartYAxis{
+                AxisMarks {value in
+                    AxisGridLine()
+                        .foregroundStyle(Color.secondary.opacity(0.3))
+                    
+                    AxisValueLabel()
+                }
+            }
+            
+            
         }.padding()
          .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
     }
